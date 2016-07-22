f35fdc3c3b69355d141ce8a245e43fb6142abeed
Sean Murphy <murp@zhaw.ch>
2016-07-22 16:52:48 +0200

    Added support for very basic scheduler which just detects if there is new work and schedules it immediately.

--- mistral/services/periodic.py
+++ mistral/services/periodic.py
@@ -25,6 +25,7 @@ from mistral.engine.rpc_backend import rpc
 from mistral import exceptions as exc
 from mistral.services import security
 from mistral.services import triggers
+from mistral.services import dtw
 
 LOG = logging.getLogger(__name__)
 
@@ -72,6 +73,39 @@ class MistralPeriodicTasks(periodic_task.PeriodicTasks):
             finally:
                 auth_ctx.set_ctx(None)
 
+
+    @periodic_task.periodic_task(spacing=1, run_immediately=True)
+    def process_delay_tolerant_workload(self, ctx):
+        """This function schedules delay tolerant workload.
+
+        In this initial, basic version, the function just determines if there
+        is new DTW and schedules it immediately.
+        """
+
+        for d in dtw.get_unscheduled_delay_tolerant_workload()
+            LOG.debug("Processing delay tolerant workload: %s" % d)
+
+            # Setup admin context before schedule triggers.
+            ctx = security.create_context(d.trust_id, d.project_id)
+
+            auth_ctx.set_ctx(ctx)
+
+            LOG.debug("Delay tolerant workload security context: %s" % ctx)
+
+            try:
+                # execute the workload
+
+                rpc.get_engine_client().start_workflow(
+                    d.workflow.name,
+                    d.workflow_input,
+                    description="DTW Workflow execution created.",
+                    **d.workflow_params
+                    )
+            except Exception:
+                # Log and continue to next cron trigger.
+                LOG.exception("Failed to process delay tolerant workload %s" % str(d))
+            finally:
+                auth_ctx.set_ctx(None)
 
 def advance_cron_trigger(t):
     modified_count = 0
